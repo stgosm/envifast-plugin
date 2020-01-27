ecd70789c1a2aad14da45c7de469013a1a31a3f2
stgosm <santiago.serrano.marquez@hotmail.com>
2020-01-27 17:29:04 -0600

    Inicializando archivos

---
+++ envifast-plugin-incio/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Envifast
+ * Author: Santiago Serrano
+ * Descroption: This is an experimental project to Add Web tech, we´re creating a plugin that can connect with Envifast page.
+ */
+
+ 
